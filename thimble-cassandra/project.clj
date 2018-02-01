d306df8810652c70fc4bd553a5a064da835bc16c
Derek Troy-West <derek@troywest.com>
2018-02-01 16:32:18 +1100

    Version 0.1.1

--- thimble-cassandra/project.clj
+++ thimble-cassandra/project.clj
@@ -1,4 +1,4 @@
-(defproject com.troy-west/thimble-cassandra "0.1.1-SNAPSHOT"
+(defproject com.troy-west/thimble-cassandra "0.1.1"
   :description "Thimble: Apache Cassandra Testing Toolkit"
 
   :license {:name "Eclipse Public License"
