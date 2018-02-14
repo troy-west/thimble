c09d74366fd20b1e83ac596f0ff8d2826b25d08a
Derek Troy-West <derek@troywest.com>
2018-02-15 10:24:55 +1100

    Version 0.1.9-SNAPSHOT

--- thimble-kafka/project.clj
+++ thimble-kafka/project.clj
@@ -1,4 +1,4 @@
-(defproject com.troy-west/thimble-kafka "0.1.8"
+(defproject com.troy-west/thimble-kafka "0.1.9-SNAPSHOT"
   :description "Thimble: Apache Kafka Testing Toolkit"
 
   :url "https://github.com/troy-west/thimble"
